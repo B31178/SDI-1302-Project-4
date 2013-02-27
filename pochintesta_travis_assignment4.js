6d09d55f62e52a57d09e6a4dbe871dd06cbec6f7
B31178 <travisp@fullsail.edu>
2013-02-27 16:34:52 -0500

    Initial layout.

--- pochintesta_travis_assignment4.js
+++ pochintesta_travis_assignment4.js
@@ -1,4 +1,29 @@
 //	Travis J. Pochintesta
 //	SDI 1302
 //	Project 4
-//	
+//  Code Library
+
+
+
+//  STRING METHODS
+
+//  Title Case
+
+//  Is URL?
+
+
+
+//  NUMBER METHODS
+
+//  String to Number
+
+//  Fuzzy Match
+
+
+
+//  ARRAY METHODS
+
+//  Smallest Value
+
+//  Total Value
+

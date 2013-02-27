5cac841f518ef1a9ee2e801be0ceab7469a2fe98
B31178 <travisp@fullsail.edu>
2013-02-27 18:28:42 -0500

    Working on URL identifier.

--- pochintesta_travis_assignment4.js
+++ pochintesta_travis_assignment4.js
@@ -4,21 +4,29 @@
 //  Code Library
 
 
-
 //  STRING METHODS
 
 //  Title Case
 var heading = "my document title.";
 
 var words = heading.split(" ");
-	for(i = 0; i < words.length; i++){
-	words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
+for (i = 0; i < words.length; i++) {
+    words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
 }
 finalTitle = words.join(" ");
 console.log(finalTitle);
 
 //  Is URL?
-
+exampleURL = "https://www.google.com/";
+splitURL = exampleURL.split("://");
+protocol = splitURL[0];
+if (protocol === "http") {
+    console.log("This is a URL.");
+} else if (protocol === "https"); {
+    console.log("This is a URL.")
+} else {
+    console.log("This is not a URL.");
+}
 
 
 //  NUMBER METHODS
@@ -34,4 +42,3 @@ console.log(finalTitle);
 //  Smallest Value
 
 //  Total Value
-

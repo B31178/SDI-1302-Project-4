1fb7b78e9ec4f0dc45ec81bd42ae56b6f463f511
B31178 <travisp@fullsail.edu>
2013-02-27 17:46:51 -0500

    Title case complete.

--- pochintesta_travis_assignment4.js
+++ pochintesta_travis_assignment4.js
@@ -8,6 +8,14 @@
 //  STRING METHODS
 
 //  Title Case
+var heading = "my document title.";
+
+var words = heading.split(" ");
+	for(i = 0; i < words.length; i++){
+	words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
+}
+finalTitle = words.join(" ");
+console.log(finalTitle);
 
 //  Is URL?
 

165922cf8c4dcd281ccb62ebdafd9555471b57f9
B31178 <travisp@fullsail.edu>
2013-02-28 14:44:13 -0500

    Complete overhaul debugged but still contains logic errors.

--- pochintesta_travis_assignment4.js
+++ pochintesta_travis_assignment4.js
@@ -1,82 +1,97 @@
+
 //	Travis J. Pochintesta
 //	SDI 1302
 //	Project 4
-//  Code Library
+//  Function Library
+myLibrary = function () {
 
+    //  STRING METHODS
 
-//  STRING METHODS
+    //  Title Case
 
+    var titleCase = function (title) {
+        var words = title.split(" ");
+        for (i = 0; i < words.length; i++) {
+            words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
+        }
+        finalTitle = words.join(" ");
+        return title
+    }
 
-//  Title Case
-var heading = "my document title.";
+    //  Is URL?
 
-var words = heading.split(" ");
-for (i = 0; i < words.length; i++) {
-    words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
+    isUrl = function (url) {
+        url = url.split("://");
+        protocol = url[0];
+        if (protocol === "http") {
+            return true;
+        } else if (protocol === "https") {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    //  NUMBER METHODS
+
+    //  String to Number
+
+    /*parseInt(number, radix) {
+            return number;
+        }*/
+
+    //  Number Formatting
+    /*var formatNum = function (number) {
+            (formatNum.toFixed(2));
+            return number
+        }*/
+
+    //  ARRAY METHODS
+
+    //  Smallest Value
+
+    var findMin = function (value, length) {
+        for (var i = 0; i < myArray.length; i++) {
+            if (i > myArray[i]) {
+                console.log(myArray[i]);
+                return value
+            }
+        }
+    }
+
+    //  Total Value
+
+    addNum = function (array, result) {
+        for (var i = 0; i < array.length; i++) {
+            if (!isNaN(parseInt(array[i]))) {
+                result += array[i];
+            }
+        }
+        return result;
+
+    }
+
+    //  Return Object
+    return {
+
+        "titleCase": titleCase,
+        "isUrl": isUrl,
+        /*"parseInt": parseInt,
+        "formatNum": formatNum,*/
+        "findMin": findMin,
+        "addNum": addNum,
+
+    }
 }
-finalTitle = words.join(" ");
-console.log(finalTitle);
 
-//  Is URL?
-exampleURL = "https://www.google.com/";
-splitURL = exampleURL.split("://");
-protocol = splitURL[0];
-if (protocol === "http") {
-    console.log("This is a URL.");
-} else if (protocol === "https") {
-    console.log("This is a URL.")
-} else {
-    console.log("This is not a URL.");
-}
+var newLib = new myLibrary();
 
-
-//  NUMBER METHODS
-
-
-//  String to Number
-var tammetNumber = "333";
-parseInt("333", 10);
-console.log(tammetNumber);
-
-//  Number Formatting !!!WRONG!!!
-var formatNum = function (myNum, adjustLength) {
-	myNum = myNum + " ";
-	myNum.length = adjustLength;
-	return myNum
-}
-console.log(formatNum(6,2));
-
-
-//  ARRAY METHODS
-
-
-//  Smallest Value  !!!WRONG!!!
 var myArray = [1, 2, 3, 5, 8, 13, 21];
+var mixedArray = [1, "One", 2, "Two", 3, "Three", 5, "Five"];
 
-var findMin = function(value, length) {
-	for (var i = 0; i < myArray.length; i++) {
-		if (i > myArray[i]) {
-		console.log(myArray[i]);
-		return value
-		}
-	}	
-}
-
-console.log(findMin(myArray[i], myArray.length));
-
-//  Total Value
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+console.log(newLib.titleCase("this is my title."));
+console.log(newLib.isUrl("https://www.google.com"));
+/*console.log(newLib.parseInt("333", 10));
+console.log(newLib.formatNum(6));*/
+console.log(newLib.findMin(myArray, myArray.length));
+console.log(newLib.addNum(mixedArray, 0));
